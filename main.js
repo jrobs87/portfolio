0e2cd30a01b733c22051b731905bf7e765edba1b
John Robertson <tiktaalikdesign@gmail.com>
2019-03-16 03:21:26 -0400

    huge improvemnts - menu hide/reveal on scroll and balanced design

--- main.js
+++ main.js
@@ -1,50 +1,74 @@
-// // A $( document ).ready() block.
-// $(document).ready(function () {
-//     setTimeout($.proxy(function() {
-//         $('#loader').css("background-color", "white");
-//         $('#loader').css("opacity", "0");
-//     }), 1200);
-    
+// Note: the loader animation and element are contained in the body of the html so they load first.
+// This is crucial in order for the loader to function.  I may move this to the main js file later and
+// enqueue it earlier in the head element
 
-//     setTimeout($.proxy(function() {
-//      $('#loader').css("display", "none");
-//     }), 2500);
+// Begin navbar border application/removal
+// Applies a bottom border to the navbar element on scroll and removes when at the top of page
+// let afterBorder = ('<div>');
+// console.log('created border');
+// $('header').append(afterBorder);
+$(document).ready(function () {
 
-//     console.log("ready!");
-// });
-// border-bottom: 1px solid black;
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();    
+    'use strict';
+
+    var c, currentScrollTop = 0,
+        navbar = $('header');
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 700) {
+            $(window).scroll(function () {
+                var a = $(window).scrollTop();
+                var b = navbar.height();
+
+                currentScrollTop = a;
+
+                if (c < currentScrollTop && a > b + b) {
+                    navbar.addClass("scrollUp");
+                    $('#right').css("right","-4em");
+                    $('#left').css("left","-4em");
+                } else if (c > currentScrollTop && !(a <= b)) {
+                    navbar.removeClass("scrollUp");
+                    $('#right').css("right","0em");
+                    $('#left').css("left","0em");
+                }
+                c = currentScrollTop;
+            })
+        }
+    });
+});
+
+
+console.log('all good so far');
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
     if (scroll >= 90) {
-        $(".navbar").css("border-bottom", "1px solid lightgrey");
+        $(".expandBorder").css("width", "100%");
     } else {
-        $(".navbar").css("border-bottom", "none");
-    }; 
+        // $(".expandBorder").css("width", "0%"); // i may end up removing this and allowing the border to stick past scroll point
+    };
+});
+// End navbar border application/removal
+
+// Begin Expand/Collapse functionality - sequence of transitions controlled here - DO NOT REORDER
+// Controls the expansion of the right sidebar 
+$("#expand").click(function () {
+    $('#right').css("background-color", "black"); //fade in sidebar/modal background first
+    $('#right').css("width", "100%"); //expand to full screen
+    $('.sidebarContent').css("visibility", "hidden"); //conceal the social icons
+    $('#contactForm').css("opacity", "1"); //reveal the contact form content
+    $('#close').css("visibility", "visible"); //reveal the close button
+    console.log('sidebar has been expanded'); //every day we commentin'
+
 });
 
-$("#expand").click(function () {    
-    $('#right').css("background-color", "black");
-    $('#right').css("width", "100%");
-    $('.sidebarContent').css("visibility", "hidden");
-    $('#contactForm').css("opacity", "1");
-    $('#close').css("visibility", "visible");
-    console.log('sidebar has been expanded');
-    
-});
-
+// Controls the collapse of the right sidebar
 $("#close").click(function () {
-    $('#close').css("visibility", "hidden");
-    $('#contactForm').css("opacity", "0");
-    $('#right').css("width", "4em");
-    $('#right').css("background-color", "white");
-    $('.sidebarContent').css("visibility", "visible");
-    console.log('sidebar has been closed');
+    $('#contactForm').css("opacity", "0"); //fade out the contact form content
+    $('#close').css("visibility", "hidden"); //hide the close button
+    $('#right').css("width", "4em"); //resets the sidebar to the original width
+    $('#right').css("background-color", "white"); //fade out modal to white
+    $('.sidebarContent').css("visibility", "visible"); //reveal the social icons (may need to set this to a timer - showing too early)
+    console.log('sidebar has been closed'); //every day we commentin'
 });
-
-
-// $(".navbar-toggler").click(function () {  
-//     $('.navbar').css("background-color", "red");
-//     console.log('sidebar has been closed');
-// });
-
-console.log('heck');
+// End Expand/Collapse functionality
